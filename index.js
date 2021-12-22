ec3bdef34a32d3e9222ad7def0dd42548216286c
Iamsej <iamsamueljones@gmail.com>
2021-12-22 18:55:21 -0500

    3.6 functional, no warnings

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -28,8 +30,68 @@ app.get('/', (request, response) => {
     response.send('<h1>Phonebook server. Are you supposed to be back here?</h1>')
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<p> Phonebook currently has ${persons.length} people in it </p> <p>${new Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = 'Person not found'
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        persons = persons.filter(person => person.id !== id);
+        response.status(204).end()
+    } else {
+        response.statusMessage = 'Person not found'
+        response.status(404).end()
+    }
+})
+
+function randInt() {
+    min = 1
+    max = 10000000
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    } else if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: randInt(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+    
 })
 
 const PORT = 3001
