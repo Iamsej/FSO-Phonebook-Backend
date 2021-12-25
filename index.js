9a8975a6274e6d4dc37a9f9dfddddaffbb69877b
Iamsej <iamsamueljones@gmail.com>
2021-12-25 01:01:57 -0500

    Online deployment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -11,6 +12,7 @@ morgan.token('postObject', function(req, res) {
     }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postObject'))
 
@@ -108,7 +110,7 @@ app.post('/api/persons', (request, response) => {
     
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
