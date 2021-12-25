c8b6d55b62169311365f380a7ba3fd9810f3d786
Iamsej <iamsamueljones@gmail.com>
2021-12-25 01:07:55 -0500

    frontend integration

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ morgan.token('postObject', function(req, res) {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postObject'))
-
+app.use(express.static('build'))
 
 
 
