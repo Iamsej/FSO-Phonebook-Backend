22775c8a9c6797d6f595948a73158fe293cb7c6b
Iamsej <iamsamueljones@gmail.com>
2022-01-04 15:20:22 -0500

    3.20 functional, no warnings

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/person')
 const res = require('express/lib/response')
 const mongoose = require('mongoose')
+const Person = require('./models/person')
 const app = express()
 
 morgan.token('postObject', function(req, res) {
@@ -16,12 +16,14 @@ morgan.token('postObject', function(req, res) {
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.error(error.name)
   
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message})
+    }
+
     next(error)
 }
 
@@ -29,7 +31,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postObject'))
 app.use(express.static('build'))
-app.use(errorHandler)
 
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook server. Are you supposed to be back here?</h1>')
@@ -76,37 +77,33 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     const person = new Person({
         name: body.name,
         number: body.number
     })
     
-    if (!body.name || !body.number) {
+    /*if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'content missing'
         })
-    }
+    }*/
     
     console.log(person.name)
     Person.exists( { name: `${person.name}`}).then((answer) => {
-        if (answer === true) {
-            console.log("Name already in phonebook")
-            response.status(400).json({
-                error: 'Name already in phonebook'
-            })
-        } else {
+        
             person.save().then(result => {
                 console.log(`added ${person.name} ${person.number} to phonebook`)
                 response.json(result)
                 })
-        }      
+                .catch(error => next(error))
+              
     })
     .catch(error => next(error))   
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     docId = request.params.id
     const body = request.body
     const person = {
@@ -114,7 +111,7 @@ app.put('/api/persons/:id', (request, response) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, {number: body.number}, {new:true})
+    Person.findByIdAndUpdate(request.params.id, {number: body.number}, {runValidators: true, context: 'query', new:true})
         .exec()
         .then(updatedPerson => {
             response.status(202).json(updatedPerson)
@@ -127,6 +124,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
